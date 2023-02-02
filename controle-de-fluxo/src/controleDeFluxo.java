40050faa7fef57665c3786c32964081ae72f94e9
pyteres <ianpimenta18@gmail.com>
2023-02-02 16:15:31 -0300

    terceira-parte

---
+++ controle-de-fluxo/src/controleDeFluxo.java
@@ -0,0 +1,17 @@
+public class controleDeFluxo {
+  public static void main(String[] args) {
+    /*
+     * Controle de fluxo: Sao estruturas que tem a capacidade de direcionar o fluxo de execuçao do codigo.
+    */
+    /*
+     * Desiçao: if if-else if-else-if switch e operador ternario.
+     * Repetiçao: for while do-while.
+     * Interrupçao: break continue e return.
+     */
+    /*Tipos
+     * Desiçao: Estrutura que avalia uma condiçao boleana ou variavel para direcioar o fluxo da execuçao.
+     * Opçoes: if(se) switch(escolha) e operador ternario.
+     */
+
+  }  
+}
