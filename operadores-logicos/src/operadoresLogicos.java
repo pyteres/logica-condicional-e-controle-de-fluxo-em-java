9aa8d18554daedce55731067a00b79df27f16722
pyteres <ianpimenta18@gmail.com>
2023-02-02 15:47:48 -0300

    segunda-parte

---
+++ operadores-logicos/src/operadoresLogicos.java
@@ -0,0 +1,31 @@
+public class operadoresLogicos {
+    public static void main(String[] args) {
+    //Operadores logicos: Sao simbolos especiais qye sao capazes de realizar comparaçoes logicas entre operandos logicos ou expreçoes retornando um resultado.
+    /*Tipos
+     * Conjunçao:
+     * Dejunçao:
+     * Dejunçao esclusiva:
+     * Negação:
+     */
+    /*
+     * Conjunçao: Expresao que so e verdadeira quando ambos os operandos ou expreçoes envolvidas sao verdadeiras.
+     * Simbologia: &&.
+     * Terminaçao: and(e)
+     */
+    /*
+     * Dijunçao: Operaçao que e so e falsa quando os dois operantes ou  expreçoes envolvidas sao falsas.
+     * Simbologia: ||;
+     * Terminaçao: or(ou);
+     */
+    /*
+     * dijunçao esclusiva: Operaçao que so e verdade quando as duas operandos ou expreçoes sao opostas.
+     * Simbologia: ^;
+     * Terminaçao: xor;
+     */
+    /*
+     * Negaçao: Operaçao que inverte o valor logico de um operando ou de uma expreçao.
+     * Simbologia: !;
+     * Terminaçao: inverçao;
+     */
+    }
+}
