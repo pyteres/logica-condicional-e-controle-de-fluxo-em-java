cb2497915ebaa25ab9b77b8cd13e08f9a53fbfb4
pyteres <ianpimenta18@gmail.com>
2023-02-02 11:44:20 -0300

    primeira-parte

---
+++ operadores-relacionais/src/operadoresRelacionais.java
@@ -0,0 +1,26 @@
+public class operadoresRelacionais {
+    public static void main(String[] args) {
+        //Operadores relacionais: Sao simbolos especiais que sao capazes de relacionar comparaçoes entre determinados operandos e em seguida retornar um resultado.
+        //Existem dois tipos de operadores relacionais o semelhante que confere se o valores sao iguais e os de grandeza que confere se os valores sao maiores ou menores.
+        /*operador de similaridade
+         * igualdade: Determina se um operador e igual ao outro.
+         * diferença: Determina se um operante e diferente do outro.
+         */
+        /*Simbologia:
+         * igualdade: ==;
+         * diferença: !;
+         */
+        /*operadores grandeza
+         * maior: Determina se o valor e maior que o outro.
+         * menor: Determina se o valor e menor que o outro.
+         * maior igual: Determina se o valor e maior ou igual ao outro valor.
+         * menor igual: Determina se o valor e menor ou igual ao outro valor.
+         */
+        /*simbolos
+         * maior: >;
+         * menor: <:
+         * mairo igual: >=;
+         * menor igual: <=
+         */
+    }
+}
