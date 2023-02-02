f6056e80b0bfc9ac6c4aa0eb9e80b40753e9d83a
pyteres <ianpimenta18@gmail.com>
2023-02-02 16:28:04 -0300

    quarta-parte

---
+++ blocos/src/bloco.java
@@ -0,0 +1,17 @@
+public class bloco {
+    public static void main(String[] args) {
+        /*objeto
+         * E um grupo de 0 ou mais codigos que trablaham em cojunto  para executar uma operaçao.
+         */
+        /*Tipos
+         * Locais: Dentro de metodos.
+         * Estaticos: Dentro de classes.
+         * Instacias: Dentro de classes.
+         */
+        /*Locais: Exemplo;
+         * public static void main(String[] args){
+         * sytem.out.print("colar palavra");
+         * }
+         */
+    }
+}
