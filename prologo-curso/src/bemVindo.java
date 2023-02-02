6299549b9e8a0fba44d6113a182d77c58f2043b8
pyteres <ianpimenta18@gmail.com>
2023-02-02 10:53:51 -0300

    Prologo-Curso

---
+++ prologo-curso/src/bemVindo.java
@@ -0,0 +1,5 @@
+public class bemVindo {
+    public static void main(String[] args) {
+        System.out.println("sejam bem-vindos a mais um curso da DIO neste curso vamos falar sobre operadores relacionais, operadores logicos, controle de fluxo e blocos");
+    }
+}
